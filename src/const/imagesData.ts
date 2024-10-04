460fc9db35a8d310231b8400cbc9ace00dc56289
MarcinOstaszewski <MarcinOstaszewski@gmail.com>
2024-10-04 15:07:58 +0200

    fix spellings;

--- src/const/imagesData.ts
+++ src/const/imagesData.ts
@@ -6,7 +6,7 @@ type ImageDataType = {
 }
 
 export const imagesData: ImageDataType[] = [
-  { id: 1, src: ["turtle-game-1.png", "turtle-game-2.png", "turtle-game-3.png"], title: "React Turtle Game", description: "Simple arcade game with animated CSS-made Turtle character." },
-  { id: 2, src: ["skirmish-battle-1.png", "skirmish-battle-2", "skirmish-battle-3"], title: "Skirmish Battle App", description: 'Team builder for Skirmish Battle table-top game.' },
-  { id: 3, src: ["sliding-tanks-1.png", "sliding-tanks-2.png", "sliding-tanks-3.png"], title: "Sliding Tanks Game", description: 'Fast, competitive game with up to 4 tasks sliding on ice.' },
+  { id: 1, src: ["turtle-game-1.png", "turtle-game-2.png", "turtle-game-3.png"], title: "React Turtle Game", description: "Simple arcade game with animated CSS-made turtle character." },
+  { id: 2, src: ["skirmish-battle-1.png", "skirmish-battle-2", "skirmish-battle-3"], title: "Skirmish Battle App", description: 'Team builder for skirmish battle table-top game.' },
+  { id: 3, src: ["sliding-tanks-1.png", "sliding-tanks-2.png", "sliding-tanks-3.png"], title: "Sliding Tanks Game", description: 'Fast, competitive game with up to 4 tanks sliding on ice.' },
 ]
